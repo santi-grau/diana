2dee273ff87014059924cd7bce65100a17d3fce7
Diego Esteban <info@diegoesteban.eu>
2014-10-24 17:59:10 +0200

    gallery ratio

--- wp-content/themes/diana/project.php
+++ wp-content/themes/diana/project.php
@@ -21,25 +21,19 @@
 <div id="main">
 
 <div class="content project clearfix">
-	<a href="javascript:void(0)" class="item c3" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-1.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
-	<a href="javascript:void(0)" class="item c3 last" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-2.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-1.jpg" width="449" height="692">
+	<span href="javascript:void(0)" class="like"></span>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-2.jpg" width="462" height="692">
+	<span href="javascript:void(0)" class="like"></span>
 	<p class="item">PHOTOGRAPHY: RAUL RUZ | STYLISM: MONICA ZAFRA | MAKE-UP&HAIR: JOSE SEQUI</p>
-	<a href="javascript:void(0)" class="item c3" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-3.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
-	<a href="javascript:void(0)" class="item c3 last" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-4.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
-	<a href="javascript:void(0)" class="item c1" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-5.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
-	<a href="javascript:void(0)" class="item c1" style="background:url(<?php bloginfo('stylesheet_directory'); ?>/img/project-6.jpg) no-repeat center center">
-		<span href="javascript:void(0)" class="like"></span>
-	</a>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-3.jpg" width="462" height="647">
+	<span href="javascript:void(0)" class="like"></span>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-4.jpg" width="447" height="676">
+	<span href="javascript:void(0)" class="like"></span>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-5.jpg"  width="1200" height="706">
+	<span href="javascript:void(0)" class="like"></span>
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/img/project-6.jpg" width="1200" height="523">
+	<span href="javascript:void(0)" class="like"></span>
 </div>
 
 <?php get_footer(); ?>
